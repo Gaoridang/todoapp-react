be3e593a1176dfadab7a59c0ced09d47c7164430
gaori <monooi@naver.com>
2023-07-22 20:07:40 +0900

    Feat: Connect firebase db

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 import { getDatabase, ref, set } from "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 interface TodoType {
   title: string;
@@ -9,6 +11,21 @@ interface TodoType {
   todoID?: Date;
 }
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDqqZ-0PBmpLKwd8xe724O_OsRRMBt4tC4",
+  authDomain: "todoapp-5edc0.firebaseapp.com",
+  databaseURL: "https://todoapp-5edc0-default-rtdb.firebaseio.com",
+  projectId: "todoapp-5edc0",
+  storageBucket: "todoapp-5edc0.appspot.com",
+  messagingSenderId: "866788428543",
+  appId: "1:866788428543:web:52ff6914ed09fba6d81aca",
+  measurementId: "G-TGDE31BPCE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function addTodo({
   todoID = new Date(),
   done = false,
@@ -17,8 +34,8 @@ function addTodo({
   description = "",
   priority = 4,
 }: TodoType) {
-  const db = getDatabase();
-  set(ref(db, "users/" + todoID), {
+  const db = getDatabase(app);
+  set(ref(db, "todos/" + todoID), {
     title,
     description,
     done,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,2 @@
 import "./style.css";
 import "./add.ts";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  
-`;
